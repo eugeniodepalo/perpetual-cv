9fd05fcfb72325d48d30e6bc46d0f4a9fbd9ba5e
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-02-20 18:00:32 +0000

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -11,9 +11,10 @@ export const data = {
   title: "Senior Full-stack Software Engineer",
   email: "eugeniodepalo@gmail.com",
   github: "eugeniodepalo",
-  description1: "I love intellectual challenges that just won't let me sleep.",
-  description2:
-    "I'm currently based in London. Open to both remote and central London contracting gigs.",
+  description2: "I love intellectual challenges that just won't let me sleep.",
+  description1:
+    "I have worked with many clients in the UK, both small and large, and either as a full-stack developer or a front-end specialist." +
+    "I have extensive experience in both worlds but I've gravitated more towards front-end technologies in recent years.",
   clients: [
     {
       name: "Featurist",
